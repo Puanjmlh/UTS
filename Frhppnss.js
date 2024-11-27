dfc2e56bd0103da1bbb79f009b959171dc87a0f6
Puanjmlh <puanjamilah31@gmail.com>
2024-11-27 07:44:30 +0700

    in

--- Frhppnss.js
+++ Frhppnss.js
@@ -1,48 +1,43 @@
 const galleryContainer = document.querySelector('.galeri-kontener');
 const galleryControlsContainer = document.querySelector('.galeri-kontrol');
-const galleryControls = ['previoues', 'next'];
+const galleryControls = ['previous', 'next'];
 const galleryItems = document.querySelectorAll('.galeri-produk');
 
 class Carousel {
-
     constructor (container, items, controls) {
         this.carouselContainer = container;
         this.carouselControls = controls;
         this.carouselArray = [...items];
     }
-
     updateGallery(){
         this.carouselArray.forEach (el => {
-            el.classlist.remove('galeri-produk-1');
-            el.classlist.remove('galeri-produk-2');
-            el.classlist.remove('galeri-produk-3');
-            el.classlist.remove('galeri-produk-4');
-            el.classlist.remove('galeri-produk-5');
-            el.classlist.remove('galeri-produk-6');
-            el.classlist.remove('galeri-produk-7');
+            el.classList.remove('galeri-produk-1');
+            el.classList.remove('galeri-produk-2');
+            el.classList.remove('galeri-produk-3');
+            el.classList.remove('galeri-produk-4');
+            el.classList.remove('galeri-produk-5');
+            el.classList.remove('galeri-produk-6');
+            el.classList.remove('galeri-produk-7');
         });
-
         this.carouselArray.slice (0, 5).forEach((el , i) => {
-            el.classlist.add (`galeri-produk-${i+1}`);
+            el.classList.add (`galeri-produk-${i+1}`);
         });
     }
-
     setCurrentState(direction){
         if (direction.className == 'galeri-kontrol-previous'){
             this.carouselArray.unshift(this.carouselArray.pop());
-        }else{
+        }else{ 
             this.carouselArray.push(this.carouselArray.shift());
         }
         this.updateGallery();
     }
-
+    
     setControls () {
         this.carouselControls.forEach(control => {
             galleryControlsContainer.appendChild(document.createElement(`button`)).className = `galeri-kontrol-${control}`;
             document.querySelector(`.galeri-kontrol-${control}`).innerText = control;
         });
     }
-
     useControls(){
         const triggers = [...galleryControlsContainer.childNodes];
         triggers.forEach(control => {
@@ -52,10 +47,9 @@ class Carousel {
             });
         });
     }
-
 }
-
 const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
 
 exampleCarousel.setControls();
+exampleCarousel.updateGallery();
 exampleCarousel.useControls();
